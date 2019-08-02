0ba9986a034fdd3c356d995cef92e16383b4cff5
Blueberry33 <53601686+Blueberry33@users.noreply.github.com>
2019-08-02 14:35:43 +0530

    Create beg55.cpp

---
+++ beg55.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int a,b,n;
+	cin>>a>>b;
+	n=a*b;
+	if(n%2==0)
+		cout<<"even";
+	else
+		cout<<"odd";
+	return(0);
+	
+}
