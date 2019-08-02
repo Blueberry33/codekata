d296c2d0e8614c42909e254473824394247670ca
Blueberry33 <53601686+Blueberry33@users.noreply.github.com>
2019-08-02 15:21:38 +0530

    Create beg26.cpp

---
+++ beg26.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,t;
+	cin>>n;
+	int a[n];
+	for(int k=0;k<n;k++)
+		cin>>a[k];//fgsfdfgsdrgsdfg
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			if(a[i]<a[j])
+			{
+				t=a[i];
+				a[i]=a[j];
+				a[j]=t;
+			}
+		}
+	}
+	for(int i=0;i<n;i++)
+	{
+		if(i==0)	
+			cout<<a[0];
+		else
+			cout<<" "<<a[i];
+	}
+	
+	return 0;
+}
