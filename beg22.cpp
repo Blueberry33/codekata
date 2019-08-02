c62c3eb26c681f570800cf039ab8859f79af3f27
Blueberry33 <53601686+Blueberry33@users.noreply.github.com>
2019-08-02 15:41:07 +0530

    Create beg22.cpp

---
+++ beg22.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,t;
+	cin>>n;
+	int a[n];
+	for(int i=0;i<n;i++)
+		cin>>a[i];
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			if(a[i]<a[j])
+			{
+				t=a[i];
+				a[i]=a[j];
+				a[j]=t;
+			}
+		}
+	}
+	cout<<a[n-1];
+	
+	return 0;
+}
