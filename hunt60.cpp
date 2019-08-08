3eed6e54d81f0e7b103bb5f99d2bf34cd656958c
Blueberry33 <53601686+Blueberry33@users.noreply.github.com>
2019-08-08 14:44:58 +0530

    Update hunt60.cpp

--- hunt60.cpp
+++ hunt60.cpp
@@ -3,26 +3,38 @@
 using namespace std;
 int main()
 {
-    int n,k,temp;
+    int n,k,temp,f=0;
     cin>>n>>k;
-    int a[n];
+    int a[n],b[n];
     for(int i=0;i<n;i++)
     {
         cin>>a[i];
     }
-    for(int j=0;j<k;j++)
+    for(int i=0;i<n;i++)
+    {
+        cin>>b[i];
+    }
+    
+    for(int j=1;;j++)
     {
         temp=a[0];
         for(int i=0;i<n-1;i++)
             a[i]=a[i+1];
         a[n-1]= temp;
-    }
-     for(int i=0;i<n;i++)
-    {
-        if(i==0)
-            cout<<a[i];
-        else
-            cout<<" "<<a[i];
-    }
+        for(int i=0;i<n;i++)
+	    {
+	       if(a[i]==b[i])
+	    	{
+	    		cout<<j;
+	    		f++;
+	    		break;
+	    	}
+	    }
+	    if(f==1)
+	    {
+	    	break;
+	    }
 
+    }
+    
 }
